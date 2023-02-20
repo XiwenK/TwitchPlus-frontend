746c55ffb58b4aa7460598786a6360b64c5e5b97
seanking <152>
2023-02-20 12:25:58 +0800

    first commit

---
+++ src/components/PageHeader.js
@@ -0,0 +1,39 @@
+// PageHeader takes props from App.js and sends signinOnSuccess to Login.js as props.
+
+import { Layout, Row, Col, Button } from "antd";
+import Favorites from "./Favorites";
+import Register from "./Register";
+import Login from "./Login";
+import React from "react";
+
+const { Header } = Layout;
+
+function PageHeader({
+  loggedIn,
+  signoutOnClick,
+  signinOnSuccess,
+  favoriteItems,
+}) {
+  return (
+    <Header>
+      <Row justify="space-between">
+        <Col>{loggedIn && <Favorites favoriteItems={favoriteItems} />}</Col>
+        <Col>
+          {loggedIn && (
+            <Button shape="round" onClick={signoutOnClick}>
+              Logout
+            </Button>
+          )}
+          {!loggedIn && (
+            <>
+              <Login onSuccess={signinOnSuccess} />
+              <Register />
+            </>
+          )}
+        </Col>
+      </Row>
+    </Header>
+  );
+}
+
+export default PageHeader;
